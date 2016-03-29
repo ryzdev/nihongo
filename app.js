3c37e081da204ec3aafd8530a43ec6511d06d1b7
Ryz Jenvey <ryszard.jenvey@autotrader.co.uk>
2016-03-29 22:58:16 +0100

    1-2

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
 angular.module('nihongo', []).controller('nihongoController', function () {
 
-    this.word = 'cheese';
 
+    var nouns = [
+        {en: 'ice cream', jp: 'aisu kurīmu', article: 'an'},
+        {en: 'this', jp: 'kore'},
+        {en: 'that', jp: 'sore'},
+        {en: 'that over there', jp: 'are'}
+    ];
+
+    var phraseData = [
+        {
+            en: 'Excuse me, please may I have INDEF NOUN?',
+            jp: 'sumimasen NOUN o kudasai'
+        },
+        {
+            en: 'Please may I have INDEF NOUN?',
+            jp: 'NOUN o kudasai'
+        }
+    ];
+
+    var phrases = [];
+    angular.forEach(phraseData, function (phrase) {
+        angular.forEach(nouns, function (noun) {
+            phrases.push(buildPhrase(phrase, noun));
+        });
     });
+    this.phrases = phrases;
+
+    function buildPhrase(phrase, noun) {
+        var builtPhrase = {};
+        builtPhrase.en = phrase.en.replace('NOUN', noun.en).replace('INDEF', noun.article ? noun.article : '');
+        builtPhrase.jp = phrase.jp.replace('NOUN', noun.jp);
+        return builtPhrase;
+    }
+
+});
+
